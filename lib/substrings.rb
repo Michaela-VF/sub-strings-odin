c9eab192219dcfb9e48b245b60d5958b95ce75c5
Michaela-VF <fouki.michaela@yahoo.com>
2026-03-22 12:57:40 +0000

    Refactors the method for speed and readability

--- lib/substrings.rb
+++ lib/substrings.rb
@@ -1,11 +1,11 @@
 
 def substrings(text, dictionary)
-    hash_of_substrings =  Hash.new(0)
     text = text.downcase
-    dictionary.each do |string|
-        string = string.downcase
-        matches = text.scan(string) # how many times #string from #dictionary is present in #text (as a subsequence of characters)
-        hash_of_substrings[string] += matches.length if matches.length > 0
+    
+    dictionary.each_with_object(Hash.new(0)) do |string, h|
+        # each_with_object is a cleaner version since we're trying to create a hash, and it automatically returns that hash!
+        strings_count = text.scan(string.downcase).length
+        h[string.downcase] += strings_count if strings_count.positive?
     end
-    hash_of_substrings
+    # so no need to return the new hash here, otherwise it raises an error!
 end
